0d74171cc24a0f1a951f561056c2c9bb9dfa739f
Caramelo <cccarlos@iesrafaelalberti.es>
2025-11-18 18:55:52 +0100

    Commit commit

--- app/src/main/java/com/example/actividad5_tema3_carloscaramecerero/screen/ItemListScreen.kt
+++ app/src/main/java/com/example/actividad5_tema3_carloscaramecerero/screen/ItemListScreen.kt
@@ -1,24 +1,17 @@
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.Button
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.sp
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.hilt.navigation.compose.hiltViewModel
-import com.example.actividad5_tema3_carloscaramecerero.viewmodel.ItemViewModel
 
 @Composable
-fun ItemListScreen(viewModel: ItemViewModel = hiltViewModel()) {
-    val items = viewModel.items.collectAsState() // Ahora StateFlow funciona directo
-
-    LazyColumn {
-        items(items.value) { item ->
-            Row {
-                Text(item.nombre)
-                Button(onClick = { /* editar */ }) { Text("Editar") }
-                Button(onClick = { viewModel.deleteItem(item.id) }) { Text("Eliminar") }
-            }
-        }
+fun ItemListScreen() {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(text = "¡Bienvenido a ItemList!", fontSize = 24.sp)
     }
 }
